3345a2bd2e826fc37a86b0212d085cda09ed0b63
Daniel <danieldourado2102@gmail.com>
2025-07-01 21:54:07 -0400

    fazendo controller de transacoes

--- src/Controller/TransacaoController.php
+++ src/Controller/TransacaoController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+
+use App\Dto\UsuarioDto;
 use App\Dto\TransacaoDto;
+use App\Dto\TransacaoRealizarDto;
 use App\Entity\Transacao;
 use App\Repository\ContaRepository;
+use App\Dto\UsuarioContaDto;
 use App\Repository\TransacaoRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,56 +17,59 @@ use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/api')]
-final class TransacoesController extends AbstractController
+final class TransacaoController extends AbstractController
 {
     #[Route('/transacoes', name: 'transacoes_realizar', methods: ['POST'])]
-    public function transferir(
+    public function realizar(
         #[MapRequestPayload(acceptFormat: 'json')]
-        TransacaoDto $transacaoDto,
-
-        EntityManagerInterface $entityManager,
-        ContaRepository $contaRepository
-    ): JsonResponse {
-
+        TransacaoRealizarDto $entrada,
+        ContaRepository $contaRepository,
+        UsuarioContaDto $usuarioContaDto
+    ): JsonResponse
+    {
         $erros = [];
-        if (!$transacaoDto->getNumeroOrigem()) {
-            $erros[] = ['message' => 'Conta de origem é obrigatória!'];
+        // validar os dados do DTO
+        if (!$entrada->getIdUsuarioOrigem()) {
+            array_push($erros, [
+                'message' => 'Conta de origem é obrigatória!'
+            ]);
         }
-        if (!$transacaoDto->getNumeroDestino()) {
-            $erros[] = ['message' => 'Conta de destino é obrigatória!'];
+        if (!$entrada->getIdUsuarioDestino()) {
+            array_push($erros, [
+                'message' => 'Conta de destino é obrigatória!'
+            ]);
         }
-        if (!$transacaoDto->getValor() || $transacaoDto->getValor() <= 0) {
-            $erros[] = ['message' => 'Valor da transação inválido!'];
+        if (!$entrada->getValor() || (float)$entrada->getValor() <= 0) {
+            array_push($erros, [
+                'message' => 'Valor deve ser valido!'
+            ]);
         }
 
-        if (count($erros) > 0) {
+        if ($entrada->getIdUsuarioOrigem() === $entrada->getIdUsuarioDestino()){
+            array_push($erros, [
+                'message' => 'As contas devem ser distintas!'
+            ]);
+        
+        }
+
+        if (count($erros) > 0){
             return $this->json($erros, 422);
         }
 
-        $contaOrigem = $contaRepository->findByNumero($transacaoDto->getNumeroOrigem());
-        $contaDestino = $contaRepository->findByNumero($transacaoDto->getNumeroDestino());
-
-        if (!$contaOrigem || !$contaDestino) {
-            return $this->json(['message' => 'Conta de origem ou destino não encontrada!'], 404);
+        $contaOrigem = $contaRepository->findByUsuarioId($entrada->getIdUsuarioOrigem());
+        if (!$contaOrigem){
+            return $this->json([
+                'message' => 'Conta de origem não encontrada'
+            ], 404);
         }
 
-        if ($contaOrigem->getSaldo() < $transacaoDto->getValor()) {
-            return $this->json(['message' => 'Saldo insuficiente!'], 400);
+        $contaDestino = $contaRepository->findByUsuarioId($entrada->getIdUsuarioDestino());
+        if (!$contaDestino){
+            return $this->json([
+                'message' => 'Conta de destino não encontrada'
+            ], 404);
         }
 
-        // Realizar a transferência
-        $contaOrigem->setSaldo($contaOrigem->getSaldo() - $transacaoDto->getValor());
-        $contaDestino->setSaldo($contaDestino->getSaldo() + $transacaoDto->getValor());
-
-        $transacao = new Transacao();
-        $transacao->setContaOrigem($contaOrigem);
-        $transacao->setContaDestino($contaDestino);
-        $transacao->setValor($transacaoDto->getValor());
-        $transacao->setData(new \DateTime());
-
-        $entityManager->persist($transacao);
-        $entityManager->flush();
-
-        return $this->json(['message' => 'Transferência realizada com sucesso!'], 201);
+        return $this->json("Hello World");
     }
 }
